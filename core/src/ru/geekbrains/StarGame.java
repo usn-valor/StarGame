36f9832af341d39a8b7de574ed5f1dec39e5b868
Arsen Sayfullin <silver.arsen@gmail.com>
2020-12-02 21:27:48 +0300

    Add Homework for Lesson 1

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -9,11 +9,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture backGroundImg;
+	int x;
+	int vX;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		backGroundImg = new Texture("Moonwalker.jpg");
+		vX = 1;
 	}
 
 	@Override
@@ -21,7 +26,13 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(backGroundImg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(img, x, 0);
+		if (x < 0)
+			vX = 1;
+		else if ((x + img.getWidth()) > Gdx.graphics.getWidth())
+			vX = -1;
+		x = x + vX;
 		batch.end();
 	}
 	
